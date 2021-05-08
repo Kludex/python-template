67bf6aa2eb28fab28e1781e29d81eb843e9fc0f0
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-05-08 14:55:07 -0700

    🔥 Remove Cython packaging (#9)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,16 +7,9 @@ def remove_docs():
     os.remove(".github/workflows/deploy-docs.yml")
 
 
-def remove_cython():
-    os.remove("build.py")
-
-
 def main():
     if "{{ cookiecutter.add_docs }}" == "False":
         remove_docs()
-
-    if "{{ cookiecutter.add_cython }}" == "False":
-        remove_cython()
 
 
 if __name__ == "__main__":
