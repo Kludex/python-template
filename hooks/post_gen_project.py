a2c36344105d455aedf116222e570f69dbef1bfc
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-21 15:30:56 +0200

    :sparkles: Add auto publish

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 
 def remove_docs():
@@ -7,9 +8,34 @@ def remove_docs():
     os.remove(".github/workflows/deploy-docs.yml")
 
 
+def git_init():
+    subprocess.call(["git", "init"])
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "-m", "🎉 First commit"])
+
+
+def github_init():
+    subprocess.call(
+        [
+            "gh",
+            "repo",
+            "create",
+            "{{ cookiecutter.project_slug }}",
+            "--public",
+            "--description",
+            "{{ cookiecutter.description }}",
+            "--confirm",
+        ]
+    )
+    subprocess.call("git", "push", "origin", "main")
+
+
 def main():
     if "{{ cookiecutter.add_docs }}" == "False":
         remove_docs()
+
+    git_init()
+    github_init()
 
 
 if __name__ == "__main__":
