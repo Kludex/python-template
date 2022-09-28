32d6e3a06352a3e7fe581aeb2e6c5ff9c9463e34
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-09-28 20:42:14 +0200

    Change backend from `setuptools` by `hatch` (#48)

--- {{ cookiecutter.project_slug }}/setup.py
+++ {{ cookiecutter.project_slug }}/setup.py
@@ -1,3 +1,26 @@
+import sys
+
 from setuptools import setup
 
-setup()
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+{{ cookiecutter.package }} no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
+
+# The below code will never execute, however GitHub is particularly
+# picky about where it finds Python packaging metadata.
+# See: https://github.com/github/feedback/discussions/6456
+#
+# To be removed once GitHub catches up.
+
+setup(
+    name="{{ cookiecutter.package }}",
+    install_requires=[],
+)
