c1ba71698a8a60b3d49efbda3ca6660d43ca7c63
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-03-01 19:08:21 +0100

    🔥 Remove cookiecutter hooks

--- hooks/post_gen_project.py
+++
@@ -1,8 +0,0 @@
-import functools
-import subprocess
-
-subprocess_call = functools.partial(subprocess.call, stdout=subprocess.DEVNULL)
-
-subprocess_call(["git", "init"])
-subprocess_call(["git", "add", "*"])
-subprocess_call(["git", "commit", "-m", "Initial commit"])
