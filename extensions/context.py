d591649e91c10083d39c7e55bf20f627de974583
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-11 00:37:05 +0200

    ✨ Load folder on generated project

--- extensions/context.py
+++ extensions/context.py
@@ -2,8 +2,9 @@ from copier_templates_extensions import ContextHook
 
 
 class ContextUpdater(ContextHook):
+    update = False
+
     def hook(self, context):
         project_name: str = context["project_name"]
-        context["project_slug"] = project_name.lower().replace(" ", "-")
-        context["package"] = project_name.lower().replace(" ", "_")
-        return context
+        project_slug = context["project_slug"] = project_name.lower().replace(" ", "-")
+        context["package"] = project_slug.replace("-", "_")
