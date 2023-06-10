9cca9f171a44aac1ce69400cceacd3988f71aafb
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-06-11 00:33:24 +0200

    ♻️ Change ContextUpdater update attribute

--- extensions/context.py
+++ extensions/context.py
@@ -2,9 +2,8 @@ from copier_templates_extensions import ContextHook
 
 
 class ContextUpdater(ContextHook):
-    update = False
-
     def hook(self, context):
         project_name: str = context["project_name"]
         context["project_slug"] = project_name.lower().replace(" ", "-")
         context["package"] = project_name.lower().replace(" ", "_")
+        return context
