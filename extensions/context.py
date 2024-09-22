4c9e25b5469e9a2732a652268e4edb90387c048f
Marcelo Trylesinski <marcelotryle@gmail.com>
2024-09-22 14:52:45 +0200

    update

--- extensions/context.py
+++ extensions/context.py
@@ -1,10 +1,13 @@
 from copier_templates_extensions import ContextHook
+from typing import Any
+import datetime
 
 
 class ContextUpdater(ContextHook):
     update = False
 
-    def hook(self, context):
-        project_name: str = context["project_name"]
+    def hook(self, context: dict[str, Any]) -> dict[str, Any]:  # type: ignore
+        project_name = context["project_name"]
         project_slug = context["project_slug"] = project_name.lower().replace(" ", "-")
         context["package"] = project_slug.replace("-", "_")
+        context["__year"] = str(datetime.datetime.today().year)
